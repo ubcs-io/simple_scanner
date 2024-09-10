05019fa4b2d76961115a8ae641341f7d7bbed385
ubcs-io <roman@ubcs.io>
2024-09-10 01:04:13 -0400

    Add logic around spawning new signals

--- main.gd
+++ main.gd
@@ -1,13 +1,23 @@
 extends Node
 
 @export var contact: PackedScene
-var score
+var score = 0
+var search_for_life = 0
+var total_contacts = 0
+var at_menu = 1
 
 func _ready() -> void:
 	pass
 
 func _process(_delta) -> void:
-	pass
+	
+	if total_contacts < 7 and at_menu == 0:
+		search_for_life = randi_range(0,5000)
+		if search_for_life < 7:
+			var contact = contact.instantiate()
+			contact.position = Vector2(randi_range(100,1100), randi_range(100,550))
+			add_child(contact)
+			total_contacts = total_contacts + 1
 
 func _increment_signals():
 	score = score + 1
@@ -15,9 +25,5 @@ func _increment_signals():
 	
 func new_game():
 	score = 0
+	at_menu = 0
 	$cursor.start($StartPosition.position)
-
-	var contact = contact.instantiate()
-	contact.position = Vector2(randi_range(200,500), randi_range(200,500))
-
-	add_child(contact)
